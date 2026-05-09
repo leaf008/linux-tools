52c4dededc8b248b75fa067c07124185bba4daf8
leaf008 <132480391+leaf008@users.noreply.github.com>
2026-05-08 21:18:21 -0700

    Create install-lite.sh

---
+++ install-lite.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/leaf008/linux-tools/main/install.sh -o /usr/local/bin/lt
+
+chmod +x /usr/local/bin/lt
+
+echo ""
+echo "================================"
+echo " Linux Tools 安装完成"
+echo "================================"
+echo ""
+echo "快捷命令："
+echo ""
+echo "lt"
+echo ""
