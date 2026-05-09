79aec7e60ccbcd2c097ce27248f084c33b00cc97
leaf008 <132480391+leaf008@users.noreply.github.com>
2026-05-09 01:24:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ echo " 5. PHP安全加固"
 echo " 6. 网站巡检"
 echo " 7. 网站备份"
 echo " 8. 网站恢复"
+echo " 9. 网站故障修复"
 echo " 0. 退出"
 echo ""
 
@@ -68,6 +69,11 @@ read -p "按回车返回菜单..."
 
 8)
 bash <(curl -fsSL https://raw.githubusercontent.com/leaf008/linux-tools/main/restore-backup.sh)
+read -p "按回车返回菜单..."
+;;
+
+9)
+bash <(curl -fsSL https://raw.githubusercontent.com/leaf008/linux-tools/main/site-fix.sh)
 read -p "按回车返回菜单..."
 ;;
 
